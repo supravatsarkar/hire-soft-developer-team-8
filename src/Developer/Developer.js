30489f776b40b1bfb9d73d8628e273d9f9550231
Supravat Sarkar <supravatsarkar18@gmail.com>
2021-09-26 18:47:51 +0530

    login implement of cart

--- src/Developer/Developer.js
+++ src/Developer/Developer.js
@@ -5,18 +5,18 @@ const Developer = (props) => {
     // console.log(props);
     const { name, age, img, salary, gender, country, role } = props.developer;
     return (
-        <div class="card developer">
+        <div className="card developer">
             <img src={img} className="card-img-top d-block mx-auto" alt="..." />
-            <div class="card-body">
-                <h5 class="card-title">{name}</h5>
-                <p class="card-text">Gender: {gender}</p>
-                <p class="card-text">Age: {age}</p>
-                <p class="card-text">Role: {role}</p>
-                <p class="card-text">Salary: ${salary}</p>
-                <p class="card-text">Country: {country}</p>
+            <div className="card-body">
+                <h5 className="card-title text-danger">{name}</h5>
+                <p className="card-text">Gender: {gender}</p>
+                <p className="card-text">Age: {age}</p>
+                <p className="card-text">Role: {role}</p>
+                <p className="card-text">Salary: ${salary}</p>
+                <p className="card-text">Country: {country}</p>
             </div>
-            <div class="card-footer">
-                <button type="button" class="btn btn-danger">Add to cart</button>
+            <div className="card-footer">
+                <button onClick={() => props.addToCartHandle(props.developer)} type="button" className="btn btn-danger">Add to cart</button>
             </div>
         </div>
 
--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -4,6 +4,7 @@ import './Main.css';
 
 const Main = () => {
     const [developers, setDevelopers] = useState([]);
+    const [items, setItems] = useState([]);
 
     useEffect(() => {
         fetch('./fakedata.json')
@@ -14,6 +15,17 @@ const Main = () => {
             });
     }, [])
 
+    const addToCartHandle = (developer) => {
+        const newArray = [...items];
+        if (!items.find(item => item.name === developer.name)) {
+            newArray.push(developer);
+        } else {
+            alert('Hey! ' + developer.name + ' Already added');
+        }
+        setItems(newArray);
+
+    }
+    // console.log(newArray);
     return (
         <div className="main">
             {/* Developers section  */}
@@ -22,13 +34,16 @@ const Main = () => {
                     developers.map(developer => <Developer
                         key={developer.name}
                         developer={developer}
+                        addToCartHandle={addToCartHandle}
                     ></Developer>)
                 }
             </div>
 
             {/* Cart Section*/}
             <div className="cart-container">
-                <h2 >Cart</h2>
+                <h2 className={"text-danger"}>Cart</h2>
+                <h5 className={"text-danger"}>Item Added{items.length}</h5>
+                {items.map(item => <h5 className={"text-danger"}>{item.name}</h5>)}
             </div>
         </div>
     );
